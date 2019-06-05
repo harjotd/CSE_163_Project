183fdc337cde815c2f0282dd811d4982e2aa927a
Harjot Dhaliwal <harjotd@uw.edu>
2019-06-04 18:49:25 -0700

    Added Spotify API

--- project.py
+++ project.py
@@ -1,2 +1,12 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import config
+
+
+def create_dataset():
+    id = config.client_id
+    sec = config.client_sec
+    client_credentials_manager = SpotifyClientCredentials(client_id=id,
+                                                          client_secret=sec)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    
